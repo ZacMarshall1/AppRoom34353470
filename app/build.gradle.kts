5bfdcafac73cff47705fd7faca26c05cb8ede920
zacMarshall <juliokretschmerb@gmail.com>
2024-11-25 17:03:20 -0300

    versão do graddle a dnroidmanifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "br.edu.up.rgm34353470"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.edu.up.rgm34353470"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
