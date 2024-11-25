7b0fd892144975d64780d0ea0045a53700a28fbe
zacMarshall <juliokretschmerb@gmail.com>
2024-11-25 16:34:19 -0300

    app inicial do codelab

---
+++ app/src/main/java/br/edu/up/rgm34353470/data/Item.kt
@@ -0,0 +1,16 @@
+package br.edu.up.rgm34353470.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity data class represents a single row in the database.
+ */
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val price: Double,
+    val quantity: Int
+)
---
+++ app/src/main/java/br/edu/up/rgm34353470/ui/AppViewModelProvider.kt
@@ -0,0 +1,52 @@
+package br.edu.up.rgm34353470.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import br.edu.up.rgm34353470.InventoryApplication
+import br.edu.up.rgm34353470.ui.home.HomeViewModel
+import br.edu.up.rgm34353470.ui.item.ItemDetailsViewModel
+import br.edu.up.rgm34353470.ui.item.ItemEditViewModel
+import br.edu.up.rgm34353470.ui.item.ItemEntryViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel(inventoryApplication().container.itemsRepository)
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
